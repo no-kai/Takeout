5647faf33344ca78050e6f04b6b3a8e1596e22e9
no-kai <kaitaisuke@gmail.com>
2022-02-11 14:56:17 +0000

    [add]　ビュー画面調整　次問い合わせ画面

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5ce558352ed580657292dcc319ad1223fa6111478353c55da12e9bfcc2f304950403d8fd93416c393783f7bbb5999595584830acacb28bd0f9d923721cfdaf67'
