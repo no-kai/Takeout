0e12c0d1f516e3d33b20b7b17433942ed68cfe5d
no-kai <kaitaisuke@gmail.com>
2022-02-12 14:44:52 +0000

    [add] 問い合わせ機能作成 環境変数設定後メアド入れる必要あり

--- db/migrate/20220212072108_create_inquiries.rb
+++ db/migrate/20220212072108_create_inquiries.rb
@@ -2,7 +2,7 @@ class CreateInquiries < ActiveRecord::Migration[5.2]
   def change
     create_table :inquiries do |t|
       t.string :name
-      t.text :body
+      t.string :message
 
       t.timestamps
     end
