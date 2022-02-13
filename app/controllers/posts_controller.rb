0d70fbfdb857f74f840144bdaeb799b5ddf254f1
no-kai <kaitaisuke@gmail.com>
2022-02-13 13:15:56 +0000

    [add]　いいね・コメント実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,12 +12,13 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
-    redirect_to posts_path
+    redirect_to post_path(@post.id)
   end
 
   def show
     @post = Post.find(params[:id])
-    @user = User.find(params[:id])
+    @comments = @post.comments
+    @comment = Comment.new
   end
 
   def edit
