0739fb272c10af9178b2121f2170745128c5ef82
no-kai <kaitaisuke@gmail.com>
2022-02-19 11:10:03 +0000

    [add]　レイアウト調整

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,26 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     @post = Post.find(params[:post_id])
-    comment = current_user.comments.new (comment_params)
-    comment.post_id = @post.id
-    comment.save
-    redirect_to post_commenrs
+    @comment = current_user.comments.new (comment_params)
+    @comment.post_id = @post.id
+    @comment.save
+    # redirect_to request.referer 非同期通信
   end
 
   def destroy
     @post = Post.find(params[:post_id])
-    comment = @post.comments.find(params[:id])
-    comment.destroy
-    redirect_to post_comments
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    # redirect_to post_path(params[:post_id]) 非同期通信
   end
 
 
   private
 
   def comment_params
-    params.require(:comment).permit(:comment)
+    params.require(:comment).permit(:body)
   end
 
 end
